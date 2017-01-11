b67fc29196f33cdf55fce70c5a8c721450ec9bd7
nanyuantingfeng <gongyanyu@ekuaibao.com>
2017-01-11 12:25:49 +0800

    1.0.2

--- src/MessageCenter.js
+++ src/MessageCenter.js
@@ -1,101 +1,98 @@
-/************************************************
- * Created By nanyuantingfeng On 5/30/16 02:44.
- ************************************************/
+/**************************************************
+ * Created by nanyuantingfeng on 20/12/2016 14:51.
+ **************************************************/
 class MessageCenter {
 
   constructor(maxListeners = null, localConsole = console) {
-    const self = this
-
-    self._callbacks = {};
-    self._events = {}
-    self._console = localConsole;
-    self._maxListeners = maxListeners === null ? null : parseInt(maxListeners, 10);
-
+    this._handlers = {};
+    this._events = {}
+    this._console = localConsole;
+    this._maxListeners = maxListeners === null ? null : parseInt(maxListeners, 10);
     return this;
   }
 
-  _addCallback(eventName, callback, context, weight) {
-    this._getCallbacks(eventName).push({ callback, context, weight });
-    this._getCallbacks(eventName).sort((a, b) => b.weight - a.weight);
+  _addHandler(eName, handler, context, weight) {
+    this._getHandlers(eName).push({ handler, context, weight });
+    this._getHandlers(eName).sort((a, b) => b.weight - a.weight);
     return this;
   }
 
-  _getCallbacks(eventName) {
-    return this._callbacks[eventName];
+  _getHandlers(eName) {
+    return this._handlers[eName];
   }
 
-  _getCallbackIndex(eventName, callback) {
-    return this._has(eventName) ?
-      this._getCallbacks(eventName)
-        .findIndex(element => element.callback === callback) : -1;
+  _getHandlerIndex(eName, handler) {
+    return this._has(eName)
+      ? this._getHandlers(eName).findIndex(element => element.handler === handler)
+      : -1;
   }
 
-  _achieveMaxListener(eventName) {
+  _achieveMaxListener(eName) {
     const self = this;
-    return (self._maxListeners !== null && self._maxListeners <= this.listenersNumber(eventName));
+    return (self._maxListeners !== null && self._maxListeners <= this.listenersNumber(eName));
   }
 
-  _callbackIsExists(eventName, callback, context) {
-    const callbackInd = this._getCallbackIndex(eventName, callback);
-    const activeCallback = callbackInd !== -1 ?
-      this._getCallbacks(eventName)[callbackInd] : void 0;
+  _handlerIsExists(eName, handler, context) {
+    const handlerInd = this._getHandlerIndex(eName, handler);
+    const activeHandler = handlerInd !== -1 ?
+      this._getHandlers(eName)[handlerInd] : void 0;
 
-    return (callbackInd !== -1 && activeCallback && activeCallback.context === context);
+    return (handlerInd !== -1 && activeHandler && activeHandler.context === context);
   }
 
-  _has(eventName) {
-    return !!this._events[eventName]
+  _has(eName) {
+    return !!this._events[eName]
   }
 
-  _on(eventName, callback, context = null, weight = 1) {
+  _on(eName, handler, context = null, weight = 1) {
     const self = this;
 
-    if (typeof callback !== 'function') {
-      throw new TypeError(`${callback} is not a function`);
+    if (typeof handler !== 'function') {
+      throw new TypeError(`${handler} is not a function`);
     }
 
     // If event wasn't added before - just add it
-    // and define callbacks as an empty object.
-    if (!this._has(eventName)) {
-      self._events[eventName] = eventName;
-      self._callbacks[eventName] = [];
+    // and define handlers as an empty object.
+    if (!this._has(eName)) {
+      self._events[eName] = eName;
+      self._handlers[eName] = [];
     } else {
       // Check if we reached maximum number of listeners.
-      if (this._achieveMaxListener(eventName)) {
-        self._console.warn(`Max listeners (${self._maxListeners}) for event "${eventName}" is reached!`);
+      if (this._achieveMaxListener(eName)) {
+        self._console.warn(`Max listeners (${self._maxListeners}) for event "${eName}" is reached!`);
       }
 
-      // Check if the same callback has already added.
-      if (this._callbackIsExists(...arguments)) {
-        self._console.warn(`Event "${eventName}" already has the callback ${callback}.`);
+      // Check if the same handler has already added.
+      if (this._handlerIsExists(...arguments)) {
+        self._console.warn(`Event "${eName}" already has the handler ${handler}.`);
       }
     }
 
-    this._addCallback(...arguments);
+    this._addHandler(...arguments);
     return this;
   }
 
-  _off(eventName, callback = null) {
+  _un(eName, handler = null) {
     const self = this;
-    let callbackInd;
-    if (this._has(eventName)) {
-      if (callback === null) {
-        self._events[eventName] = undefined;
-        delete self._events[eventName]
-        self._callbacks[eventName] = null;
+    let handlerInd;
+    if (this._has(eName)) {
+      if (handler === null) {
+        self._events[eName] = undefined;
+        delete self._events[eName]
+        self._handlers[eName] = null;
       } else {
-        callbackInd = this._getCallbackIndex(eventName, callback);
-        if (callbackInd !== -1) {
-          this._getCallbacks(eventName).splice(callbackInd, 1);
-          this._off(...arguments);
+        handlerInd = this._getHandlerIndex(eName, handler);
+        if (handlerInd !== -1) {
+          this._getHandlers(eName).splice(handlerInd, 1);
+          this._un(...arguments);
         }
       }
     }
     return this;
   }
 
-  _emit(eventName) {
-    const custom = this._callbacks[eventName];
+  _emit(eName) {
+    const custom = this._handlers[eName];
     let i = custom ? custom.length : 0;
     let len = arguments.length;
     let args;
@@ -108,58 +105,83 @@ class MessageCenter {
     while (i--) {
       current = custom[i];
       if (arguments.length > 1) {
-        current.callback.apply(current.context, args);
+        current.handler.apply(current.context, args);
       } else {
-        current.callback.apply(current.context);
+        current.handler.apply(current.context);
       }
     }
     args = null;
     return this;
   }
 
-  on(...args) {
-    let eventName = args.shift();
-    eventName.split("|").forEach(e => e && this._on.call(this, e, ...args));
+  /*******************************************
+   * 注册监听器("aaa|bbb|ccc")
+   * @param eName
+   * @param handler
+   * @param context
+   * @param weight
+   * @returns {MessageCenter}
+   */
+  on(eName, handler, context = null, weight = 1) {
+    eName.split("|").forEach(e => e && this._on.call(this, e, handler, context, weight));
     return this;
   }
 
-  once(eventName, callback, context = null, weight = 1) {
-    const onceCallback = (...args) => {
-      this.off(eventName, onceCallback);
-      return callback.apply(context, args);
+  /*********************************************
+   * 单次监听
+   * @param eName
+   * @param handler
+   * @param context
+   * @param weight
+   * @returns {MessageCenter}
+   */
+  once(eName, handler, context = null, weight = 1) {
+    const fn = (...args) => {
+      this.un(eName, fn);
+      return handler.apply(context, args);
     };
-    return this.on(eventName, onceCallback, context, weight);
+    return this.on(eName, fn, context, weight);
   }
 
-  off(...args) {
-    let eventName = args.shift();
-    eventName.split("|").forEach(e => e && this._off.call(this, e, ...args));
+  /*********************************************
+   * 解除监听
+   * @param eName
+   * @param args
+   * @returns {MessageCenter}
+   */
+  un(eName, ...args) {
+    eName.split("|").forEach(e => e && this._un.call(this, e, ...args));
     return this;
   }
 
-  emit(...args) {
-    let eventName = args.shift();
-    eventName.split("|").forEach(e => e && this._emit.call(this, e, ...args));
+  /**********************************************
+   * 触发监听
+   * @param eName
+   * @param args
+   * @returns {MessageCenter}
+   */
+  emit(eName, ...args) {
+    eName.split("|").forEach(e => e && this._emit.call(this, e, ...args));
     return this;
   }
 
-  fire() {
-    return this.emit.apply(this, arguments)
-  }
-
-  un() {
-    return this.off.apply(this, arguments);
-  }
-
+  /******************************************
+   * 清空当前实例中所有的监听
+   * @returns {MessageCenter}
+   */
   clear() {
-    const self = this;
-    self._events = {};
-    self._callbacks = {};
+    this._events = {};
+    this._handlers = {};
     return this;
   }
 
+  /****************************************
+   * 检测监听器的长度
+   * @param eventName
+   * @returns {number}
+   */
   listenersNumber(eventName) {
-    return this._has(eventName) ? this._callbacks[eventName].length : null;
+    return this._has(eventName) ? this._handlers[eventName].length : 0;
   }
 }
 
